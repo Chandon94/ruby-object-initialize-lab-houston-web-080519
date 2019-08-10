2b5f4638ce3fba6daf4394a52f9ed62e011da380
Chandon Porter <cneilporter94@gmail.com>
2019-08-10 02:42:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,2 @@
 class Dog
-  def name = (name)
-    @name = name
-  end 
   
-  def breed = (breed)
-    @breed = breed
-  end 
-  
-  def name = (name)
-    @name = name
-    
-  end 
-  
-  def breed = (breed)
-    @breed = breed
-  end
-  
-  def name
-    @name
-  end
-  
-  def breed
-    @breed
-  end
-end
-
-lassie = Dog.new("Mutt")
-lassie.breed
