06a515289fbef8c782e71f84e54a041ff06fa216
Chandon Porter <cneilporter94@gmail.com>
2019-08-10 02:52:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
 class Dog
   
-  def initialize(name)
+  def initialize(name, breed, = "Mutt")
     @name = name
   end
   
-    def name= (name)
-      @name = name
-    end
-    
-    def name
-      @name
-    end 
+  def name
+    @name
+  end
+  
+  def breed 
+    @breed
+  end
+
   end
   
